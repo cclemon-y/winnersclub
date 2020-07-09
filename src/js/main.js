d074be7991709bf6eedc74ffa343c16f83971645
大谷優輔 <otaniyusuke@Cclemon-MacBook-Pro.local>
2020-07-09 23:38:38 +0900

    共通化

---
+++ src/js/main.js
@@ -0,0 +1,13 @@
+function header(rootDir){
+
+    $.ajax({
+        url: rootDir + "include/header.html", // ディレクトリー変更
+        cache: false,
+        async: false,
+        dataType: 'html',
+        success: function(html){
+            html = html.replace(/\{\$root\}/g, rootDir); 
+            document.write(html);
+        }
+    });
+}
